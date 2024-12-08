b4228f1f6d0d761efd26a795648adc905686b7a3
FlozyTess <cecilrency23@gmail.com>
2024-12-09 01:23:45 +0300

    added DOM elements & array

---
+++ index.js
@@ -0,0 +1,7 @@
+//select DOM elements
+const itemInput=document.getElementById('item-input');
+const addButton=document.getElementById('add-button');
+const clearButton=document.getElementById('clear-button');
+const shoppingList=docment.getElementById('shopping-list');
+//array to store shopping list
+let shoppinglist=[];
