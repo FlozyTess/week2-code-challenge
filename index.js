0b8a0e53c45ee866ae562ffcc0d75404a6d0d5a4
FlozyTess <cecilrency23@gmail.com>
2024-12-09 01:40:51 +0300

    added the event listener function

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const addButton=document.getElementById('add-button');
 const clearButton=document.getElementById('clear-button');
 const shoppingList=docment.getElementById('shopping-list');
 //array to store shopping list
-let shoppinglist=[];
+let items=[];
+//add items to list
+addButton.addEventListener('click',() => {
+    const itemName=itemInput.value.trim();
+ if (itemName){
+    const item = {name: itemName, purchased: false };
+    items.push(item);
+    renderList();
+    itemInput.value='';
+}
+});
